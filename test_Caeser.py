9dbfd32e901960a57685ddabea4240bcdaabdc41
Julian <julian.schuerz@students.fh-wels.at>
2020-11-10 18:38:55 +0100

    Python Programmname geändert

---
+++ test_Caeser.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 10 18:28:39 2020
+
+@author: Julian
+"""
+
+from Caeser import caesar
+def test_caeser():
+    assert caeser("Hslh qhjah lza",7) == Alea jacta est
