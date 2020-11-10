79143a73cc85fd8cc5d13933917aa32c952dc84f
Julian <julian.schuerz@students.fh-wels.at>
2020-11-10 18:58:04 +0100

    add python programm and change test file name

---
+++ test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 10 18:28:39 2020
+
+@author: Julian
+"""
+
+from Caeser import caesar
+from scalar import scalar
+
+def test():
+    assert caesar("Hslh qhjah lza",7) == print("ALEA JACTA EST")
+    assert scalar([1,2],[1,2]) == 5
