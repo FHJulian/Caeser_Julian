727753fe779bc75fad3924cdbf733c053d4f6dac
Daniel Huppmann <dh@dergelbesalon.at>
2020-12-30 13:59:11 +0100

    fix the tests

--- test.py
+++ test.py
@@ -9,8 +9,8 @@ from Caeser import caesar
 from Scalar import scalar
 
 def test_caesar():
-    assert caesar("Hslh qhjah lza",7) == print("ALEA JACTA ESE")
+    assert caesar("Hslh qhjah lza",7) == "ALEA JACTA ESE"
 
 def test_scalar():
-    return scalar([1,2],[1,2]) == 4
+    assert scalar([1,2],[1,2]) == 4
 
