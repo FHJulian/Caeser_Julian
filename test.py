e6f3ab64d37efd7ecb13ee561b3a6064d8680773
Daniel Huppmann <dh@dergelbesalon.at>
2020-12-30 13:52:39 +0100

    changes to check whether tests fail

--- test.py
+++ test.py
@@ -9,8 +9,8 @@ from Caeser import caesar
 from Scalar import scalar
 
 def test_caesar():
-    assert caesar("Hslh qhjah lza",7) == print("ALEA JACTA EST")
+    assert caesar("Hslh qhjah lza",7) == print("ALEA JACTA ESE")
 
 def test_scalar():
-    return scalar([1,2],[1,2]) == 5
+    return scalar([1,2],[1,2]) == 4
 
